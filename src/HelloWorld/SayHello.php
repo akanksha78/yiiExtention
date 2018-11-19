94fed2d50ff68ec3c8e6b992d566b8e5a7d78436
Akanksha Maurya <akankshamaurya@virtual.local>
2018-11-19 13:17:56 +0530

    Initial commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace HelloWorld;
+
+   class SayHello {
+      public static function world() {
+         return 'Hello World, Composer!';
+      }
+   }
